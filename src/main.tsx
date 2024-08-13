ba03bfe2f869161fb3b795ed2d5ceb953d2e8ff8
Ricardo <ricardodiasmorais@gmail.com>
2024-08-13 19:04:54 +0100

    fixed provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { SecretProvider } from './contexts/SecretsContext.tsx';
 import { CustomThemeProvider } from './contexts/CustomThemeContext.tsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CustomThemeProvider>
-        <SecretProvider>
-          <App />
-        </SecretProvider>
-      </CustomThemeProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <CustomThemeProvider>
+          <SecretProvider>
+            <App />
+          </SecretProvider>
+        </CustomThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
