7213285872f100d11a14af09ce618f0a0b2a28db
Ricardo <ricardodiasmorais@gmail.com>
2025-07-12 13:46:29 +0100

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { CustomThemeProvider } from '@contexts/CustomThemeContext.tsx';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
